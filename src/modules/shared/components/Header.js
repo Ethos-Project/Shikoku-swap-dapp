f7faf81e1c0a113fc5b7cfb4327af998f0ec0216
soft-top-notc <kyle.lopz29@gmail.com>
2021-08-06 17:35:28 -0700

    Updated logo

--- src/modules/shared/components/Header.js
+++ src/modules/shared/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core';
-import logo from '../../../assets/images/logo.png';
+import logo from '../../../assets/images/WebHeader.png';
 
 const Header = () => {
   const styles = useStyles();
